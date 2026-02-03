1d818c10d46e37682d2a53a4280dbd32961fcc25
chungman <leecm940603@gmail.com>
2026-02-03 14:24:49 +0900

    chore: 추가 기초 세팅

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      // 2. "@"를 "src" 폴더 전체로 매핑
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
