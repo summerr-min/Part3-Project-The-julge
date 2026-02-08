15b5a20ae58c80849a26475665e7a1f7e07798b0
chungman <leecm940603@gmail.com>
2026-02-08 18:29:25 +0900

    상세 필터 버튼 스타일 색상 선언 수정

--- src/components/list/DetailFilterButton/DetailFilterButton.styles.ts
+++ src/components/list/DetailFilterButton/DetailFilterButton.styles.ts
@@ -5,9 +5,9 @@ export const Button = styled.button`
   align-items: center;
   padding: 1.2rem;
   height: 3rem;
-  background-color: var(--red-30);
+  background-color: ${({ theme }) => theme.colors.red30};
   font-size: 1.4rem;
   font-weight: 700;
-  color: var(--white);
+  color: ${({ theme }) => theme.colors.white};
   border-radius: 0.5rem;
 `;
