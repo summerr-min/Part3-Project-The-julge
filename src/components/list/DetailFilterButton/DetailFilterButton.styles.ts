31d1de3575fbfb38c1b536cfdbfe92dc01e55d09
chungman <leecm940603@gmail.com>
2026-02-08 18:23:53 +0900

    feat: 상세 필터 버튼(DetailFilterButton) 구현

---
+++ src/components/list/DetailFilterButton/DetailFilterButton.styles.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  display: flex;
+  align-items: center;
+  padding: 1.2rem;
+  height: 3rem;
+  background-color: var(--red-30);
+  font-size: 1.4rem;
+  font-weight: 700;
+  color: var(--white);
+  border-radius: 0.5rem;
+`;
