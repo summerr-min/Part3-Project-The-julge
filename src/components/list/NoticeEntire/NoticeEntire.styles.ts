d88e8382490cd3501f0e7a1c2bd79cf0493c4fe7
chungman <leecm940603@gmail.com>
2026-02-10 16:17:42 +0900

    feat: 전체 공고 구현

---
+++ src/components/list/NoticeEntire/NoticeEntire.styles.ts
@@ -0,0 +1,43 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  display: flex;
+  gap: 1.5rem;
+  align-items: flex-start;
+  flex-direction: column;
+  min-width: 35.1rem;
+
+  @media (min-width: 768px) {
+    gap: 3.2rem;
+    min-width: 68rem;
+  }
+
+  @media (min-width: 1200px) {
+    gap: 3.2rem;
+    min-width: 96.4rem;
+  }
+`;
+
+export const SectionContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  width: 100%;
+
+  @media (min-width: 768px) {
+    justify-content: space-between;
+    flex-direction: row;
+  }
+
+  @media (min-width: 1200px) {
+    justify-content: space-between;
+    flex-direction: row;
+  }
+`;
+
+export const SectionName = styled.h2`
+  font-size: 2.8rem;
+  font-weight: 700;
+  color: var(--black);
+  letter-spacing: 0.056rem;
+`;
